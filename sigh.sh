5952bf041994ae36db3bf90982999b57cdd421b4
atec <root@atec.pub>
2018-09-11 11:19:13 -0400

    extract variables and dedup cmd

--- sigh.sh
+++ sigh.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
 ISS="Y82E2K77P5"
+EXP="1546305557"
+ETC="/keybase/private/atec/etc"
 
 if [ "$1" = "mapkit" ]; then
-    step crypto jwt sign --alg=ES256 --key=/keybase/private/atec/etc/mapkitjs/AuthKey_YKVC29UG5H.p8 --iss=$ISS --kid=YKVC29UG5H --exp=1546305557 --subtle
+    KID="YKVC29UG5H"
+    KEY="$ETC/mapkitjs/AuthKey_$KID.p8" 
 elif [ "$1" = "musickit" ]; then
-    step crypto jwt sign --alg=ES256 --key=/keybase/private/atec/etc/musickitjs/AuthKey_CUG44HA5T5.p8 --iss=$ISS --kid=CUG44HA5T5 --exp=1546305557 --subtle
+    KID="CUG44HA5T5"
+    KEY="$ETC/musickitjs/AuthKey_$KID.p8"
 else 
     echo "Must specify either mapkit or musickit"
+    exit
 fi
+
+step crypto jwt sign --alg=ES256 --key=$KEY --iss=$ISS --subtle
