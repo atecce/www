bddd840e5a215eeb7e9083474db18e5d9ba1769f
atec <root@atec.pub>
2018-09-11 11:32:41 -0400

    get way too clever

--- sign.sh
+++ sign.sh
@@ -2,14 +2,17 @@
 
 ISS="Y82E2K77P5"
 EXP="1546305557"
+
 ETC="/keybase/private/atec/etc"
+CAT="kitjs/AuthKey_"
+EXT=".p8"
 
 if [ "$1" = "map" ]; then
     KID="YKVC29UG5H"
-    KEY="$ETC/$1kitjs/AuthKey_$KID.p8" 
+    KEY="$ETC/$1$CAT$KID$EXT"
 elif [ "$1" = "music" ]; then
     KID="CUG44HA5T5"
-    KEY="$ETC/$1kitjs/AuthKey_$KID.p8"
+    KEY="$ETC/$1$CAT$KID$EXT"
 else 
     echo "Must specify either map or music"
     exit
