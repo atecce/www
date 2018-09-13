2108d2eeab4d50d96bf5d64fcbff009848bac01f
atec <root@atec.pub>
2018-09-13 14:58:00 -0400

    extract to handler

--- svc.go
+++ svc.go
@@ -2,27 +2,41 @@ package main
 
 import (
 	"log"
+	"net/http"
+	"time"
 
 	"github.com/sideshow/apns2/token"
 )
 
-func main() {
+const kid = "CUG44HA5T5"
 
-	authKey, err := token.AuthKeyFromFile("/keybase/private/atec/etc/musickitjs/AuthKey_CUG44HA5T5.p8")
+func handler(w http.ResponseWriter, r *http.Request) {
+
+	// TODO don't hit the disk on every req
+	authKey, err := token.AuthKeyFromFile("/keybase/private/atec/etc/musickitjs/AuthKey_" + kid + ".p8")
 	if err != nil {
 		log.Fatal("reading p8 file", err)
 	}
 
 	token := &token.Token{
-		AuthKey: authKey,
+		AuthKey:  authKey,
+		IssuedAt: time.Now().Unix(),
 		//		"exp":   1546305557,
-		KeyID:  "YKVC29UG5H",
+		KeyID:  kid,
 		TeamID: "Y82E2K77P5",
 	}
 
 	if ok, _ := token.Generate(); ok {
-		println("Bearer", token.Bearer)
+		log.Println("Bearer", token.Bearer)
+		w.Write([]byte(token.Bearer))
 	} else {
 		log.Println("generating token", err)
+		w.WriteHeader(http.StatusInternalServerError)
 	}
+}
+
+func main() {
+
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
