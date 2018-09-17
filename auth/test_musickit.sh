bfd4e9c3b5325568b809e1f72110ebc85bef283d
atec <root@atec.pub>
2018-09-17 18:46:48 -0400

    switch smoke test to https. rm dep on httpie

--- auth/test_musickit.sh
+++ auth/test_musickit.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-ADDR=$(gcloud compute addresses describe auth --region us-east1 --format='value(address)')
+ADDR="https://$(gcloud compute addresses describe auth --region us-east1 --format='value(address)')"
 echo $ADDR
 
-BEARER=$(http $ADDR/music)
+BEARER=$(curl -k $ADDR/music)
 echo $BEARER
 
 curl -v -H "Authorization: Bearer $BEARER" "https://api.music.apple.com/v1/catalog/us/songs/203709340"
