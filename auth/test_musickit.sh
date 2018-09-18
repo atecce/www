1297632b0496e7638d58088c779c6f0611999ad3
atec <root@atec.pub>
2018-09-17 21:43:40 -0400

    break out auth svc

--- auth/test_musickit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ADDR="https://$(gcloud compute addresses describe auth --region us-east1 --format='value(address)')"
-echo $ADDR
-
-BEARER=$(curl -k $ADDR/music)
-echo $BEARER
-
-curl -v -H "Authorization: Bearer $BEARER" "https://api.music.apple.com/v1/catalog/us/songs/203709340"
