bb33071ee1b573fa1da75c8b9f1c2050dfe375e3
atec <root@atec.pub>
2018-11-13 21:57:53 -0500

    finish Vue poc

--- App.vue
+++ App.vue
@@ -20,78 +20,17 @@
 
       <h2 class="subtitle">Dogmatic opinions. Pragmatic decisions</h2>
       <h2 class="subtitle">Stay curious</h2>
-
-      <button class="button fas fa-backward" v-on:click="backward()"></button>
-      <button class="button fas fa-play" v-on:click="play()"></button>
-      <button class="button fas fa-pause" v-on:click="pause()"></button>
-      <button class="button fas fa-forward" v-on:click="forward()"></button>
-      <button class="button" v-on:click="setQueue()">Set Queue</button>
     </div>
-    <!-- <script src="common.js"></script> -->
   </div>
 </template>
 
 <script>
 export default {
-  data: function () {
-    return {
-      music: null,
-      token: null
-    }
-  },
-
-  methods: {
-
-    // player
-    // TODO refactor next two to toggle with backing state
-    play: function () {
-      window.MusicKit.getInstance().play()
-    },
-    pause: function () {
-      this.music.pause()
-    },
-    forward: function () {
-      this.music.skipToNextItem()
-    },
-    backward: function () {
-      this.music.skipToPreviousItem()
-    },
-    setQueue: function () {
-      this.music.setQueue({
-        album: '429020349'
-      })
-    },
-  },
-
   created: function () {
-
-    document.addEventListener('musickitloaded', function () {
-
-      var xhr = new XMLHttpRequest()
-      xhr.overrideMimeType("text/plain; charset=x-user-defined")
-      xhr.open("GET", "https://auth.atec.pub/music", false)
-      xhr.send()
-      this.token = xhr.responseText
-
-      // eslint-disable-next-line
-      console.log("configuring MusicKit")
-
-      // eslint-disable-next-line
-      console.log(this.token)
-
-      window.MusicKit.configure({
-        developerToken: this.token,
-        app: {
-          name: 'atec.pub',
-          build: '0.1.0'
-        },
-      })
-
-      this.music = window.MusicKit.getInstance()
-
-      // eslint-disable-next-line
-      console.log(this.music)
-    })
+    var xhr = new XMLHttpRequest()
+    xhr.overrideMimeType("text/plain; charset=x-user-defined")
+    xhr.open("GET", "https://auth.atec.pub/", false)
+    xhr.send()
   }
 }
 </script>
--- common.js
+++
@@ -1,5 +0,0 @@
-// this is simply to record hits. all it does is log your ip
-var xhr = new XMLHttpRequest();
-xhr.overrideMimeType("text/plain; charset=x-user-defined");
-xhr.open("GET", "https://auth.atec.pub");
-xhr.send();
