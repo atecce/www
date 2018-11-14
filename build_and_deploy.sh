0f5d978d9db184abfc35d0b17a972dd1f01e1c46
atec <root@atec.pub>
2018-11-13 22:16:48 -0500

    fix: stop index.html from being overwritten

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -3,4 +3,4 @@
 vue build
 rm -rf /keybase/private/atec,kbpbot/www/*
 cp -r dist/ /keybase/private/atec,kbpbot/www/
-cp *.html *.pdf /keybase/private/atec,kbpbot/www/
+cp *.pdf /keybase/private/atec,kbpbot/www/
