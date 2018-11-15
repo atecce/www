b31d2ec33bf4b8fdd4f6f032a12e3e609a111b37
atec <root@atec.pub>
2018-11-15 17:32:02 -0500

    stay adventurous

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -3,4 +3,5 @@
 vue build
 rm -rf /keybase/private/atec,kbpbot/www/*
 cp -r dist/ /keybase/private/atec,kbpbot/www/
-cp *.pdf /keybase/private/atec,kbpbot/www/
+cp resume.pdf /keybase/private/atec,kbpbot/www/
+cp adventures.html /keybase/private/atec,kbpbot/www/
