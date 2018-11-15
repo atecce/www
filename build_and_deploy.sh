7f60acf7886d34b16dffe70abc1662da6e7f472f
atec <root@atec.pub>
2018-11-15 18:04:51 -0500

    cp favicon

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -3,5 +3,4 @@
 vue build
 rm -rf /keybase/private/atec,kbpbot/www/*
 cp -r dist/ /keybase/private/atec,kbpbot/www/
-cp resume.pdf /keybase/private/atec,kbpbot/www/
-cp adventures.html /keybase/private/atec,kbpbot/www/
+cp favicon.ico resume.pdf adventures.html /keybase/private/atec,kbpbot/www/
