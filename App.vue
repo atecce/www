df1fe5362ea2b38d96846d6e4eeeebcbd7c3c817
atec <root@atec.pub>
2018-11-15 18:20:52 -0500

    use pdf instead of html

--- App.vue
+++ App.vue
@@ -3,16 +3,16 @@
     <div class="container">
       <nav class="navbar" role="navigation" aria-label="main navigation">
         <div class="navbar-brand">
-          <a class = "navbar-item" href="https://keybase.io/atec" target="_blank">
+          <a class="navbar-item" href="https://keybase.io/atec" target="_blank">
             <i class="fab fa-keybase"></i>
           </a>
-          <a class = "navbar-item" href="https://github.com/atecce" target="_blank">
+          <a class="navbar-item" href="https://github.com/atecce" target="_blank">
             <i class="fab fa-github"></i>
           </a>
-          <a class = "navbar-item" href="https://linkedin.com/in/atecce" target="_blank">
+          <a class="navbar-item" href="https://linkedin.com/in/atecce" target="_blank">
             <i class="fab fa-linkedin"></i>
           </a>
-          <a class="navbar-item" target="_blank" href="/resume.html">
+          <a class="navbar-item" target="_blank" href="/resume.pdf">
             <i class="fas fa-file-alt"></i>
           </a>
         </div>
