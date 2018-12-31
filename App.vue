6a71627b5d180197c3f8a2fa10bf38cdb03c9da4
atec <root@atec.pub>
2018-12-31 15:11:16 -0600

    poc for d3

--- App.vue
+++ App.vue
@@ -24,11 +24,57 @@
         </div>
       </nav>
     </div>
+    <svg width="500" height="270">
+      <g style="transform: translate(0, 10px)">
+        <path :d="line" />
+      </g>
+    </svg>
   </div>
 </template>
 
 <script>
+import * as d3 from 'd3';
+
 export default {
+  data: function () {
+    return {
+      data: [99, 71, 78, 25, 36, 92],
+      line: '',
+    }
+  },
+  methods: {
+    getScales() {
+
+      // set ranges of x and y axis
+      const x = d3.scaleLinear().range([0, 430]);
+      const y = d3.scaleLinear().range([210, 0]);
+
+      // scale left and top axis
+      d3.axisLeft().scale(x);
+      d3.axisTop().scale(y);
+
+      // ???
+      x.domain(d3.extent(this.data, (d, i) => i));
+      y.domain([0, d3.max(this.data, d => d)]);
+
+      return { x, y };
+    },
+
+    calculatePath() {
+      const scale = this.getScales();
+
+      // ???
+      const path = d3.line()
+        .x((d, i) => scale.x(i))
+        .y(d => scale.y(d));
+
+      this.line = path(this.data);
+    }
+  },
+
+  mounted: function () {
+    this.calculatePath();
+  },
   created: function () {
 
     var xhr = new XMLHttpRequest()
@@ -56,3 +102,12 @@ export default {
   }
 }
 </script>
+
+<style>
+svg {
+  margin: 25px;
+  fill: none;
+  stroke: #76BF8A;
+  stroke-width: 3px;
+}
+</style>
