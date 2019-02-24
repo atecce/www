9ef512d08a12b4727679b1af2558333746e205c8
atec <root@atec.pub>
2019-02-24 08:56:31 -0500

    expand cloudflare call to multiple lines

--- ci/build_and_deploy.sh
+++ ci/build_and_deploy.sh
@@ -12,4 +12,6 @@ osascript ./ci/build_resume.applescript
 cp -r dist/ $WWW
 cp favicon.ico resume.pdf adventures.html $WWW
 
-http DELETE https://api.cloudflare.com/client/v4/zones/28774264c76780dbe367bcbe762edf66/purge_cache X-Auth-Email:root@atec.pub X-Auth-Key:$(cat /keybase/private/atec/etc/cloudflare/token) purge_everything:=true
+http DELETE https://api.cloudflare.com/client/v4/zones/28774264c76780dbe367bcbe762edf66/purge_cache \
+    X-Auth-Email:root@atec.pub X-Auth-Key:$(cat /keybase/private/atec/etc/cloudflare/token) \
+    purge_everything:=true
