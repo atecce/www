40bc806c8fe0074b1ca89707e7f6c4ac8b636d04
atec <root@atec.pub>
2019-03-02 14:38:16 -0500

    wrap x axis labels

--- Chart.vue
+++ Chart.vue
@@ -93,6 +93,7 @@ export default {
 
       d3.json(url.replace(/\(/g, "%28").replace(/\)/g, "%29")).then(function(entities) {
 
+        // convert map to array of objects and sort
         var data = []
         for (var entity in entities) {
           if (entities.hasOwnProperty(entity)) {
@@ -104,6 +105,7 @@ export default {
           return b.count - a.count
         })
 
+        // create chart
         const width = data.length * 75, height = 500
 
         d3.select(".chart")
@@ -146,12 +148,39 @@ export default {
             .attr("height", d => y(0) - y(d.count))
         
         svg.append("g")
-          .call(xAxis)
+            .call(xAxis)
+          .selectAll(".tick text")
+            .call(function(text, width) {
+              text.each(function() {
+                var text = d3.select(this),
+                    words = text.text().split(/\s+/).reverse(),
+                    word,
+                    line = [],
+                    lineNumber = 0,
+                    lineHeight = 1.1, // ems
+                    y = text.attr("y"),
+                    dy = parseFloat(text.attr("dy")),
+                    tspan = text.text(null).append("tspan").attr("x", 0).attr("y", y).attr("dy", dy + "em");
+
+                // eslint-disable-next-line
+                while (word = words.pop()) {
+                  line.push(word);
+                  tspan.text(line.join(" "));
+                  if (tspan.node().getComputedTextLength() > width) {
+                    line.pop();
+                    tspan.text(line.join(" "));
+                    line = [word];
+                    tspan = text.append("tspan").attr("x", 0).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
+                  }
+                }
+              })
+            }, x.bandwidth())
         
         svg.append("g")
           .call(yAxis)
       })
     },
+    
   },
 }
 </script>
