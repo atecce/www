7a0d323926b54576acff687382105bb53923f1d7
atec <root@atec.pub>
2019-03-11 15:53:25 -0400

    put graph and gif in container

--- ci/build_and_deploy.sh
+++ ci/build_and_deploy.sh
@@ -12,8 +12,8 @@ osascript ./ci/build_resume.applescript
 cp -r dist/ $WWW
 cp favicon.ico resume.pdf adventures.html crawl.gif $WWW
 
-cp $WWW/index.html canon
-cp $WWW/index.html io
+cp $WWW/index.html $WWW/canon
+cp $WWW/index.html $WWW/io
 
 http DELETE https://api.cloudflare.com/client/v4/zones/28774264c76780dbe367bcbe762edf66/purge_cache \
     X-Auth-Email:root@atec.pub X-Auth-Key:$(cat /keybase/private/atec/etc/cloudflare/token) \
--- components/Canon.vue
+++ components/Canon.vue
@@ -4,19 +4,18 @@
     <section class="hero">
       <div class="hero-body">
         <div class="container">
-          <h1 class="title">
-            canon 
-          </h1>
-          <h2 class="subtitle">
-            an attempt to get meaning out of text
-          </h2>
+
+          <h1 class="title">canon </h1>
+          <h2 class="subtitle">an attempt to get meaning out of text</h2>
+
+          <img src="../crawl.gif">
+
+          <Chart></Chart>
+
         </div>
       </div>
     </section>
 
-    <img src="../crawl.gif">
-
-    <Chart></Chart> 
 
   </div>
 </template>
