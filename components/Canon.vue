2399650431694eb3027546ae4151df61a8973fcb
atec <root@atec.pub>
2019-03-18 14:03:08 -0400

    add installation and clone instructions

--- components/Canon.vue
+++ components/Canon.vue
@@ -8,6 +8,10 @@
           <h1 class="title">canon </h1>
           <h2 class="subtitle">an attempt to get meaning out of text</h2>
 
+          <code>git clone https://atec.pub/canon.git</code>
+          <br><br>
+          <code>go get atec.pub/canon</code>
+          <br><br>
           <img src="../crawl.gif">
 
           <Chart></Chart>
